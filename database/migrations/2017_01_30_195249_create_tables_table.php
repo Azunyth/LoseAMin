80164606582c9adeb1d07eed2df52d1c52954e16
Azunyth <alexis.vernot@gmail.com>
2017-01-30 20:12:42 +0100

    Add table model

---
+++ database/migrations/2017_01_30_195249_create_tables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tables', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('max_seat')->default(6);
+            $table->integer('seats_available')->default(0);
+            $table->integer('min_bet')->default(2);
+            $table->timestamp('last_activity')->nullable();
+            $table->boolean('is_closed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tables');
+    }
+}
